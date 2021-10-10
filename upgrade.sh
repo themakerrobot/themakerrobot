5ba4fa5be8738e719b4b75714fe175879fe0a56c
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2021-10-10 16:15:22 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+VERSION='#20211010v1'
 
-#bash <(curl -s https://raw.githubusercontent.com/themakerrobot/themakerrobot/main/upgrade.sh)
+echo "OS update" $VERSION
+echo -n "Remove config.js? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+  echo "not Removed"
+else
+  echo "Removed config.json"
+  rm /home/pi/config.json
+fi
+#pip3 install --upgrade openpibo-python -y
 
+MODEL=$(tr -d '\0' </proc/device-tree/model)
+echo $MODEL "detected"
+if [[ $MODEL == *"Pi 3"* ]];then
+  cat > /etc/modprobe.d/snd-i2smic-rpi.conf<<EOF
+options snd-i2smic-rpi rpi_platform_generation=1
+EOF
+fi
 
-pip3 install --upgrade openpibo-python
+echo -n "REBOOT NOW? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+  echo "Exiting without reboot."
+  exit 0
+fi
+
+echo "Reboot started..."
+reboot
+exit 0
