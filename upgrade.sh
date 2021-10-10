a23b3fce483ce016fdbbf2bf1ea030ed6cc51e36
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2021-10-10 16:27:32 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,21 +6,33 @@ echo "OS update" $VERSION
 echo -n "Remove config.js? [y/N] "
 read
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
-  echo "not Removed config.json"
+  echo "Not removed config.json"
 else
   echo "Removed config.json"
   rm /home/pi/config.json
 fi
 
-echo "Update openpibo-python"
-pip3 install --upgrade openpibo-python -y
+echo -n "Update openpibo-python package? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+  echo "Not update openpibo-python"
+else
+  echo "Update openpibo-python package"
+  pip3 install --upgrade openpibo-python -y
+fi
 
-MODEL=$(tr -d '\0' </proc/device-tree/model)
-echo $MODEL "detected"
-if [[ $MODEL == *"Pi 3"* ]];then
-  cat > /etc/modprobe.d/snd-i2smic-rpi.conf<<EOF
-options snd-i2smic-rpi rpi_platform_generation=1
-EOF
+echo -n "Update MIC setting? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+  echo "Not update MIC setting"
+else
+  echo "Update openpibo-python package"
+  MODEL=$(tr -d '\0' </proc/device-tree/model)
+  echo $MODEL "detected"
+  if [[ $MODEL == *"Pi 3"* ]];then
+    cat > /etc/modprobe.d/snd-i2smic-rpi.conf<<EOF
+  options snd-i2smic-rpi rpi_platform_generation=1
+  EOF
 fi
 
 echo -n "REBOOT NOW? [y/N] "
