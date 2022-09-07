76836dfbee1843539941403ee28130fb4bda1bd1
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2022-09-07 18:27:53 +0900

    Update main.sh

--- update/main.sh
+++ update/main.sh
@@ -14,6 +14,7 @@ wget https://github.com/themakerrobot/themakerrobot/releases/download/220905v1/o
 wget https://github.com/themakerrobot/themakerrobot/releases/download/220905v1/openpibo-tools-${VERSION}.zip
 unzip openpibo-files-${VERSION}.zip
 unzip openpibo-tools-${VERSION}.zip
+sudo chown pi:pi -R openpibo-files openpibo-tools
 sudo rm openpibo-files-${VERSION}.zip openpibo-tools-${VERSION}.zip
 
 sudo shutdown -r now
