12c418400bcac4c82ea70f715ea316656ce8e24a
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2022-09-07 17:22:28 +0900

    Update main.sh

--- update/main.sh
+++ update/main.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo openpibo OS version is "$(cat /home/pi/.OS_VERSION)"
 
-echo "openpibo-file4"
+
+VERSION="220905v1"
+
+cd /home/pi
+
+sudo pip3 install python-multipart
+sudo npm install --save multer
+
+sudo rm openpibo-files openpibo-tools -rf
+wget https://github.com/themakerrobot/themakerrobot/releases/download/220905v1/openpibo-files-${VERSION}.zip
+wget https://github.com/themakerrobot/themakerrobot/releases/download/220905v1/openpibo-tools-${VERSION}.zip
+unzip openpibo-files-${VERSION}.zip
+unzip openpibo-tools-${VERSION}.zip
+sudo rm openpibo-files-${VERSION}.zip openpibo-tools-${VERSION}.zip
+
+sudo shutdown -r now
