6d4820349bd426063471be2909fa7cf9d09299ee
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2022-09-21 11:39:03 +0900

    Update main.sh

--- update/main.sh
+++ update/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo openpibo OS version is "$(cat /home/pi/.OS_VERSION)"
 
-
-VERSION="220905v1"
+F_VERSION="220905v1"
+T_VERSION="220905v1"
 
 cd /home/pi
 
@@ -10,11 +10,11 @@ sudo pip3 install python-multipart
 sudo npm install --save multer
 
 sudo rm openpibo-files openpibo-tools -rf
-wget https://github.com/themakerrobot/themakerrobot/releases/download/${VERSION}/openpibo-files-${VERSION}.zip
-wget https://github.com/themakerrobot/themakerrobot/releases/download/${VERSION}/openpibo-tools-${VERSION}.zip
-unzip openpibo-files-${VERSION}.zip
-unzip openpibo-tools-${VERSION}.zip
+wget https://github.com/themakerrobot/themakerrobot/releases/download/${F_VERSION}/openpibo-files-${F_VERSION}.zip
+wget https://github.com/themakerrobot/themakerrobot/releases/download/${T_VERSION}/openpibo-tools-${T_VERSION}.zip
+unzip openpibo-files-${F_VERSION}.zip
+unzip openpibo-tools-${T_VERSION}.zip
 sudo chown pi:pi -R openpibo-files openpibo-tools
-sudo rm openpibo-files-${VERSION}.zip openpibo-tools-${VERSION}.zip
+sudo rm openpibo-files-${F_VERSION}.zip openpibo-tools-${T_VERSION}.zip
 
 sudo shutdown -r now
