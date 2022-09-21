f32e82f04e79f554a71e2f3ed330d92ce68fe3cf
leeyunjai82 <68631024+leeyunjai82@users.noreply.github.com>
2022-09-21 14:04:18 +0900

    Update main.sh

--- update/main.sh
+++ update/main.sh
@@ -4,11 +4,18 @@ echo openpibo OS version is "$(cat /home/pi/.OS_VERSION)"
 F_VERSION="220905v1"
 T_VERSION="220921v1"
 
-cd /home/pi
 
+sudo pkill python3
+sudo pkill node
+
+sudo echo "#20:255,0,0!" >/dev/ttyS0
+
+cd /home/pi
 sudo pip3 install -U openpibo-python
 sudo pip3 install python-multipart
 sudo npm install --save multer
+
+sudo echo "#20:0,255,0!" >/dev/ttyS0
 
 wget -O openpibo-tools.zip https://github.com/themakerrobot/openpibo-tools/archive/refs/tags/${T_VERSION}.zip
 wget -O openpibo-files.zip https://github.com/themakerrobot/openpibo-files/archive/refs/tags/${F_VERSION}.zip
@@ -22,5 +29,6 @@ mv openpibo-tools-${T_VERSION} openpibo-tools
 
 sudo chown pi:pi -R openpibo-files openpibo-tools
 sudo rm openpibo-files.zip openpibo-tools.zip
+sudo echo "#20:0,0,255!" >/dev/ttyS0
 
 sudo shutdown -r now
