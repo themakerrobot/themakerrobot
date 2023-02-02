06d86b502eb8ec0f78436685056024c87577bf83
Yunjai, Lee <leeyunjai@circul.us>
2023-02-02 17:22:14 +0900

    Update pre-update.sh

--- update/pre-update.sh
+++ update/pre-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VER=$(cat /home/pi/.OS_VERSION)
 
-if [ "$VER" != "OPENPIBO_230201" ]
+if [ "$VER" != "OPENPIBO_230201_V1" ]
 then
   sudo echo "#20:200,50,50!" > /dev/ttyS0
   sleep 0.5
@@ -26,7 +26,7 @@ then
   echo 'SHELLINABOX_ARGS="--no-beep --disable-ssl --disable-ssl-menu"' >> /home/pi/shellinabox
   sudo mv -f /home/pi/shellinabox /etc/default/shellinabox
 
-  echo "OPENPIBO_230201" > /home/pi/.OS_VERSION
+  echo "OPENPIBO_230201_V1" > /home/pi/.OS_VERSION
   sudo echo "#20:0,0,0!" > /dev/ttyS0
   sudo rm -rf /home/pi/update
   sudo shutdown -r now
